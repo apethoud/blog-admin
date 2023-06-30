3352188eb5c7f1ab5a72a761d4378c20bf687951
Andrew Pethoud <andrewpethoud@gmail.com>
2023-06-30 16:44:34 -0700

    feat(CreateNewPost): Add header and minor styling adjustments to AddImages page

--- app/_UI-components/PageHeader.tsx
+++ app/_UI-components/PageHeader.tsx
@@ -1,6 +1,6 @@
 export default function PageHeader({ text }) {
   return (
-    <div className="text-lg">
+    <div className="text-lg font-bold text-slate-900 dark:text-slate-100 my-4">
       {text}
     </div>
   )
--- app/create-new-post/AddImages.tsx
+++ app/create-new-post/AddImages.tsx
@@ -1,10 +1,12 @@
+import PageHeader from "../_UI-components/PageHeader"
 import { InProgressPost } from "./interfaces"
 
 export default function AddImages({ post }: { post: InProgressPost }) {
   return (
     <div className="flex flex-col">
+      <PageHeader text="Add Images (coming soon)" />
       {post?.paragraphs.map((paragraph: string, index: number) => (
-        <div key={index} className="bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-100 m-4 p-4">{paragraph.body}</div>
+        <div key={index} className="bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-100 my-2 p-4">{paragraph.body}</div>
       ))}
     </div>
   )
