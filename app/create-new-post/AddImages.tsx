7ee522b6a0f1bced411481c40f8086b186191daa
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-05 16:04:44 -0700

    refactor(CreateNewPost): route to Posts route after form submission

--- app/create-new-post/AddImages.tsx
+++ app/create-new-post/AddImages.tsx
@@ -1,16 +1,22 @@
+import { Dispatch, SetStateAction } from "react"
 import { Paragraph, Paragraphs, Post } from "./interfaces"
 import PageHeader from "../_UI-components/PageHeader"
 import Button from "../_UI-components/Button"
 
-export default function AddImages({ paragraphs, post, submitPost }: { paragraphs: Paragraphs, post: Post, submitPost: (post: Post) => Promise<void> }) {
+export default function AddImages({ paragraphs, post, submitPost }: { paragraphs: Paragraphs, post: Post, submitPost: (paragraphs: Paragraphs, post: Post) => void }) {
+  const handleSubmit = e => {
+    e.preventDefault()
+    submitPost(paragraphs, post)
+  }
   return (
     <div className="flex flex-col">
-      <form action={() => submitPost(paragraphs, post)}>
-      <PageHeader text="Add Images (coming soon)" />
-      {paragraphs.map((paragraph: Paragraph, index: number) => (
-        <div key={index} className="bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-100 my-2 p-4">{paragraph.body}</div>
-      ))}
-      <Button primary type="submit" label="Post to Production" />
+      <form onSubmit={handleSubmit}>
+        <PageHeader text="Add Images (coming soon)" />
+        <div>{post.title}</div>
+        {paragraphs.map((paragraph: Paragraph, index: number) => (
+          <div key={index} className="bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-100 my-2 p-4">{paragraph.body}</div>
+        ))}
+        <Button primary type="submit" label="Post to Production" />
       </form>
     </div>
   )
--- app/create-new-post/page.tsx
+++ app/create-new-post/page.tsx
@@ -1,14 +1,23 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from "next/headers"
-import CreateNewPostForm from "./CreateNewPostForm";
-import { Paragraphs, Post } from "./interfaces";
-import { createParagraphRowsWithPostIdsAdded } from "../utils";
+'use client';
 
-export default async function CreateNewPost() {
-  
+import { InProgressParagraphs, Paragraphs, Post } from "./interfaces"
+import { useEffect, useState } from "react"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { createParagraphRowsWithPostIdsAdded } from "../utils";
+import AddText from "./AddText"
+import { InProgressPost } from "./interfaces"
+import AddImages from "./AddImages"
+import { useRouter } from "next/navigation";
+
+export default function CreateNewPost() {
+  const [ newPost, setNewPost ] = useState<InProgressPost>({})
+  const [ newParagraphs, setNewParagraphs ] = useState<InProgressParagraphs>([])
+  const [ step, setStep ] = useState(1)
+
+  const router = useRouter()
+
   const submitPost = async (paragraphs: Paragraphs, post: Post) => {
-    'use server'
-    const supabase = createServerComponentClient({ cookies })
+    const supabase = createClientComponentClient()
     const { data: postData, error: postError } = await supabase
       .from('posts')
       .insert([
@@ -27,9 +36,29 @@ export default async function CreateNewPost() {
         if (paragraphsError) {
           console.log("paragraphsError is: ", paragraphsError);
         }
+        if (paragraphsData) {
+          router.push("/")
+        }
     }
   }
+
   return (
-    <CreateNewPostForm submitPost={submitPost} />
+    <>
+      {step === 1 && (
+        <AddText 
+          setNewParagraphs={setNewParagraphs}
+          setNewPost={setNewPost}
+          setStep={setStep}
+          step={step}
+        />
+      )}
+      {step === 2 && (
+        <AddImages 
+          paragraphs={newParagraphs}
+          post={newPost}
+          submitPost={submitPost}
+        />
+      )}
+    </>
   )
 }
