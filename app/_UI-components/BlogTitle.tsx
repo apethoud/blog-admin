9b40f74e9eff460c4468d79bab84f9d32220bef2
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-17 15:01:25 -0700

    feat(ViewPost): add margin above blog title

--- app/_UI-components/BlogTitle.tsx
+++ app/_UI-components/BlogTitle.tsx
@@ -1,6 +1,6 @@
 export default function BlogTitle({ children }) {
   return (
-    <div className="font-sans font-bold text-5xl text-violet-600 dark:text-violet-500 antialiased my-6">
+    <div className="font-sans font-bold text-5xl text-violet-600 dark:text-violet-500 antialiased mt-12 mb-6">
       {children}
     </div>
   )
