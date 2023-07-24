fe8fe137d11a9d89ded70abd7a46dce96db1265d
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-24 14:56:41 -0700

    fix(PostCard): add type to children prop

--- app/posts/PostCard.tsx
+++ app/posts/PostCard.tsx
@@ -1,4 +1,6 @@
-export default function PostCard({ children }) {
+import { ReactNode } from "react";
+
+export default function PostCard({ children }: { children: ReactNode }) {
   return (
     <div className="border border-solid border-black dark:border-white mb-2 p-4">
       {children}
