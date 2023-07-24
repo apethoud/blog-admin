7c40ba12938f7df5d19179c88bf2fa44f6dd491a
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-24 13:18:29 -0700

    fix(headers): type children

--- app/_UI-components/H3.tsx
+++ app/_UI-components/H3.tsx
@@ -1,4 +1,6 @@
-export default function H3({ children }) {
+import { ReactNode } from "react";
+
+export default function H3({ children }: { children: ReactNode}) {
   return (
     <div className="text-xl font-bold text-slate-900 dark:text-slate-100 my-4">
       {children}
