3861581936b8f9d79c429bd5c381a3d5cd4a4421
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-24 13:11:13 -0700

    fix(BlogTitle): type children prop

--- app/_UI-components/BlogTitle.tsx
+++ app/_UI-components/BlogTitle.tsx
@@ -1,4 +1,6 @@
-export default function BlogTitle({ children }) {
+import { PropsWithChildren, ReactNode } from "react";
+
+export default function BlogTitle({ children }: { children: ReactNode }) {
   return (
     <div className="font-sans font-bold text-5xl text-violet-600 dark:text-violet-500 antialiased mt-12 mb-6">
       {children}
