c7aa4d45e87bc97b18f486c1882beaa0864a3136
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-27 11:35:16 -0700

    feat(Paragraph): add support for numbered lists

--- app/_UI-components/Paragraph.tsx
+++ app/_UI-components/Paragraph.tsx
@@ -5,10 +5,11 @@ import H2 from "./H2"
 import H3 from "./H3"
 import Li from "./ListItem"
 import Ul from "./UnorderedList"
+import Ol from "./OrderedList"
 
 export default function Paragraph({ body }: { body: string }) {
   console.log("body is: ", body)
   return (
-    <ReactMarkdown components={{ p: Text, h1: H1, h2: H2, h3: H3, ul: Ul, li: Li }}>{body}</ReactMarkdown>
+    <ReactMarkdown components={{ p: Text, h1: H1, h2: H2, h3: H3, ul: Ul, ol: Ol, li: Li }}>{body}</ReactMarkdown>
   )
 }
